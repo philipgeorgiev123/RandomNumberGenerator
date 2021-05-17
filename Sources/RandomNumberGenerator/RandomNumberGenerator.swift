9c206bb8884ba18510a70c492747c6bb81c52941
Filip Georgiev <philip.georgiev@productmadness.com>
2021-05-17 13:50:13 +0100

    random 5s

--- Sources/RandomNumberGenerator/RandomNumberGenerator.swift
+++ Sources/RandomNumberGenerator/RandomNumberGenerator.swift
@@ -4,4 +4,8 @@ public struct RandomNumberGenerator {
     public func HelloToYou()->String {
         return "Hello To You!"
     }
+    
+    public func GenerateRandomNumber()->Int {
+        return 5
+    }
 }
