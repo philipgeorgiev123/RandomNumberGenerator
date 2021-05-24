25cd852333bf19f4ddfb84486a06150733bfc1e1
Filip Georgiev <philip.georgiev@productmadness.com>
2021-05-24 12:16:12 +0100

    using numbers A

--- Package.swift
+++ Package.swift
@@ -13,16 +13,21 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:philipgeorgiev123/NumbersA.git", from: "0.1.2"),
+        
+        // .package(url: "git@github.com:philipgeorgiev123/NumbersA.git", from: "0.1.0"),
+        // .package(url: "https://example.com/example-package.git", "1.2.3"..<"1.2.6"),
+        // .package(url: "http://example.com.com/ExamplePackage/ExamplePackage", from: "1.2.3"),
+        // .package(url: "git@github.com:philipgeorgiev123/NumbersA.git", .exact("0.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RandomNumberGenerator",
-            dependencies: []),
+            dependencies: ["NumbersA"]),
         .testTarget(
             name: "RandomNumberGeneratorTests",
-            dependencies: ["RandomNumberGenerator"]),
+            dependencies: ["RandomNumberGenerator", "NumbersA"]),
     ]
 )
--- Sources/RandomNumberGenerator/RandomNumberGenerator.swift
+++ Sources/RandomNumberGenerator/RandomNumberGenerator.swift
@@ -1,15 +1,19 @@
+import NumbersA
+
 public struct RandomNumberGenerator {
     var text = "Hello, World!"
+    var numbers = NumbersA()
     
     public func HelloToYou()->String {
         return "Hello To You!"
     }
     
     public init () {
-        
+        let hasNumbers = numbers.GotNumbers()
+        print(hasNumbers)
     }
     
     public func GenerateRandomNumber()->Int {
-        return 5
+        return 5;
     }
 }
